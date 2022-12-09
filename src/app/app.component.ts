9efaf00c58ecdf4307b961a2185aad4076e42284
mishrarampravesh <rampravesh.mishra99@gmail.com>
2022-12-09 12:00:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+ selector: 'app-root',
+
+ templateUrl: './app.component.html',
+
+ styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
-  title = 'PaymentForm';
+
+ userInput1 = "";
+
+ userInput2 = "";
+
+ userInput3 = "";
+
+ onInputReceiverName(vada: Event) {
+
+  this.userInput1 = (vada.target as HTMLInputElement).value
+
+  console.log(this.userInput1)
+
+ }
+
+ onInputPaymentDate(vada: Event) {
+
+  this.userInput2 = (vada.target as HTMLInputElement).value
+
+  console.log(this.userInput2)
+
+ }
+
+ onInputAmount(vada: Event) {
+
+  this.userInput3 = (vada.target as HTMLInputElement).value
+
+  console.log(this.userInput3)
+
+ }
+
 }
